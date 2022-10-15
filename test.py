e8978d5f8b1e24a550060ab14ef26a27088d43c9
ndthien150 <43144832+ndthien150@users.noreply.github.com>
2022-10-15 22:01:14 +0700

    Update test.py

--- test.py
+++ test.py
@@ -15,11 +15,12 @@
 #     st.write("Stopped")
 
 import streamlit as st
-from webcam import webcam
 
-captured_image = webcam()
-if captured_image is None:
-    st.write("Waiting for capture...")
-else:
-    st.write("Got an image from the webcam:")
-    st.image(captured_image)
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
